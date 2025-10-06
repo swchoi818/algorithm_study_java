a5b9ae0f243346011ffa66e676f0987b27ff2ab1
swChoi <chsw030818@naver.com>
2025-10-06 22:48:28 +0900

    백준 2805 나무 자르기(parametric search)

---
+++ src/baekjoon_2805/Main.java
@@ -0,0 +1,41 @@
+package baekjoon_2805;
+
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		int[] treeHights = new int[N];
+		int high = 0;
+		int low = 0;
+		int result = 0;
+		for(int i = 0; i < N; i++) {
+			treeHights[i] = Integer.parseInt(st.nextToken());
+			if(high < treeHights[i])
+				high = treeHights[i];
+		}
+		while(low <= high) {
+			int mid = (low + high) / 2;
+			int sum = 0;
+			for(int i = 0; i < N; i++)
+				if(mid < treeHights[i])
+					sum += (treeHights[i] - mid);
+			if(sum >= M) {
+				result = mid;
+				low = mid + 1;
+			}
+			else {
+				high = mid - 1;
+			}
+		}
+		System.out.println(result);
+		br.close();
+	}
+}
