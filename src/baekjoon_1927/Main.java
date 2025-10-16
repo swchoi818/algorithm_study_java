2ccb01c8d23f9dbb7c92169d4c4b663bd79e620b
swChoi <chsw030818@naver.com>
2025-10-16 22:58:45 +0900

    백준 1927 최소 힙

---
+++ src/baekjoon_1927/Main.java
@@ -0,0 +1,37 @@
+package baekjoon_1927;
+
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N = sc.nextInt();
+		PriorityQueue<Integer> pq = new PriorityQueue();
+		for(int i = 0; i < N; i++) {
+			int x = sc.nextInt();
+			
+			if(x==0) {
+				if(pq.size() > 0) {	
+					bw.write(String.valueOf(pq.poll()));
+				}
+				else {
+					bw.write("0");
+				}
+				bw.newLine();
+			}
+			else {
+				pq.add(x);
+			}
+		}
+		
+		sc.close();
+		bw.flush();
+		bw.close();
+	}
+
+}
+
